78c2783cec5bdf98efa2827981e625e769998ba5
Thiago_19 Notebook <riffothiago19@gmail.com>
2025-09-26 12:04:27 -0300

    Agregando todo xd

--- Js/Main.js
+++ Js/Main.js
@@ -5,6 +5,11 @@ const mainListaNormal = document.getElementById('ListaNormal')
 const mainListaAdmin = document.getElementById('ListaAdmin')
 const mainLogin = document.getElementById('Login')
 const mainReporte = document.getElementById('Reporte')
+const mainCarga = document.getElementById('Carga')
+
+const Header = document.getElementById('Header')
+const Footer = document.getElementById('Footer')
+
 
 
 const styleTag = document.getElementById('styles')
@@ -20,13 +25,25 @@ const buttonMoreHome = document.getElementById('buttonMoreHome')
 const VerProyecto1 = document.getElementById('VerProyecto1')
 const VerProyecto2 = document.getElementById('VerProyecto2')
 const VerProyecto3 = document.getElementById('VerProyecto3')
+const Editar1 = document.getElementById('Editar1')
+const Editar2 = document.getElementById('Editar2')
+const Editar3 = document.getElementById('Editar3')
 
+const buttonProyectoIndividual1 = document.getElementById('buttonProyectoIndividual1')
+const buttonProyectoIndividual2 = document.getElementById('buttonProyectoIndividual2')
+const buttonProyectoIndividual3 = document.getElementById('buttonProyectoIndividual3')
 
-const mains = [mainHome, mainRanking, mainProyectos, mainListaNormal, mainListaAdmin, mainLogin, mainReporte]
+const Ver1 = document.getElementById('Ver1')
+const Ver2 = document.getElementById('Ver2')
+const Ver3 = document.getElementById('Ver3')
+
+const UsuarioLogin = document.getElementById('UsuarioLogin')
+
+const mains = [mainHome, mainRanking, mainProyectos, mainListaNormal, mainListaAdmin, mainLogin, mainReporte, mainCarga]
 
 
 aRanking.addEventListener('click', () => mostrarMain('Ranking', mains))
-aListaNormal.addEventListener('click', () => mostrarMain('ListaNormal', mains))
+aListaNormal.addEventListener('click', () => mostrarMain('ListaAdmin', mains))
 aListaNormalFooter.addEventListener('click', () => mostrarMain('ListaNormal', mains))
 aHome.addEventListener('click', () => mostrarMain('Home', mains))
 aHomeFooter.addEventListener('click', () => mostrarMain('Home', mains))
@@ -36,6 +53,17 @@ buttonMoreHome.addEventListener('click', () => mostrarMain('ListaNormal', mains)
 VerProyecto1.addEventListener('click', () => mostrarMain('Proyectos', mains))
 VerProyecto2.addEventListener('click', () => mostrarMain('Proyectos', mains))
 VerProyecto3.addEventListener('click', () => mostrarMain('Proyectos', mains))
+buttonProyectoIndividual1.addEventListener('click', () => mostrarMain('Proyectos', mains))
+buttonProyectoIndividual2.addEventListener('click', () => mostrarMain('Proyectos', mains))
+buttonProyectoIndividual3.addEventListener('click', () => mostrarMain('Proyectos', mains))
+Editar1.addEventListener('click', () => mostrarMain('Carga', mains))
+Editar2.addEventListener('click', () => mostrarMain('Carga', mains))
+Editar3.addEventListener('click', () => mostrarMain('Carga', mains))
+Ver1.addEventListener('click', () => mostrarMain('Proyectos', mains))
+Ver2.addEventListener('click', () => mostrarMain('Proyectos', mains))
+Ver3.addEventListener('click', () => mostrarMain('Proyectos', mains))
+
+UsuarioLogin.addEventListener('click', () => mostrarMain('Login', mains))
 
 
 
@@ -52,8 +80,9 @@ function mostrarMain(mainAMostrar, mains) {
     } else {
       element.classList.add('none')
     }
-    if (mainAMostrar === 'Login') {
-      mostrarLogin()
+    if (element.id === `Login`) {
+      Header.classList.add('none')
+      Footer.classList.add('none')
     }
   })
 }
