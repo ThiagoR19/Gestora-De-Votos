663289583cd21903cee34d6d7fa12f067e6f6358
Thiago_19 Notebook <riffothiago19@gmail.com>
2025-09-26 11:33:42 -0300

    Agregando botones funcionales

--- Js/Main.js
+++ Js/Main.js
@@ -17,8 +17,11 @@ const aHomeFooter = document.getElementById('aHomeFooter')
 const aReporte = document.getElementById('aReporte')
 const buttonListaHome = document.getElementById('buttonListaHome')
 const buttonMoreHome = document.getElementById('buttonMoreHome')
+const VerProyecto1 = document.getElementById('VerProyecto1')
+const VerProyecto2 = document.getElementById('VerProyecto2')
+const VerProyecto3 = document.getElementById('VerProyecto3')
 
-buttonListaHome
+
 const mains = [mainHome, mainRanking, mainProyectos, mainListaNormal, mainListaAdmin, mainLogin, mainReporte]
 
 
@@ -30,6 +33,10 @@ aHomeFooter.addEventListener('click', () => mostrarMain('Home', mains))
 aReporte.addEventListener('click', () => mostrarMain('Reporte', mains))
 buttonListaHome.addEventListener('click', () => mostrarMain('ListaNormal', mains))
 buttonMoreHome.addEventListener('click', () => mostrarMain('ListaNormal', mains))
+VerProyecto1.addEventListener('click', () => mostrarMain('Proyectos', mains))
+VerProyecto2.addEventListener('click', () => mostrarMain('Proyectos', mains))
+VerProyecto3.addEventListener('click', () => mostrarMain('Proyectos', mains))
+
 
 
 for (let i = 0; i < mains.length; i++) {
@@ -37,7 +44,7 @@ for (let i = 0; i < mains.length; i++) {
 }
 mains[0].classList.remove('none')
 
-function mostrarMain (mainAMostrar, mains) {
+function mostrarMain(mainAMostrar, mains) {
   styleTag.setAttribute('href', `./Styles/${mainAMostrar}.css`)
   mains.forEach(element => {
     if (element.id === `${mainAMostrar}`) {
