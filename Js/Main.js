8fc4f7e8cffb43ad6c7ac40c7c71fafee67ddee1
ThiagoR19 <riffothiago19@gmail.com>
2025-09-30 12:00:30 -0300

    Llamando al json para mostrar el home y los proyectos normales. y Corrigiendo errores del login

--- Js/Main.js
+++ Js/Main.js
@@ -10,8 +10,6 @@ const mainCarga = document.getElementById('Carga')
 const Header = document.getElementById('Header')
 const Footer = document.getElementById('Footer')
 
-
-
 const styleTag = document.getElementById('styles')
 
 const aRanking = document.getElementById('aRanking')
@@ -41,7 +39,6 @@ const UsuarioLogin = document.getElementById('UsuarioLogin')
 
 const mains = [mainHome, mainRanking, mainProyectos, mainListaNormal, mainListaAdmin, mainLogin, mainReporte, mainCarga]
 
-
 aRanking.addEventListener('click', () => mostrarMain('Ranking', mains))
 aListaNormal.addEventListener('click', () => mostrarMain('ListaAdmin', mains))
 aListaNormalFooter.addEventListener('click', () => mostrarMain('ListaNormal', mains))
@@ -65,8 +62,6 @@ Ver3.addEventListener('click', () => mostrarMain('Proyectos', mains))
 
 UsuarioLogin.addEventListener('click', () => mostrarMain('Login', mains))
 
-
-
 for (let i = 0; i < mains.length; i++) {
   mains[i].classList.add('none')
 }
@@ -77,18 +72,144 @@ function mostrarMain(mainAMostrar, mains) {
   mains.forEach(element => {
     if (element.id === `${mainAMostrar}`) {
       element.classList.remove('none')
-      Header.classList.remove('none')
-      Footer.classList.remove('none')
     } else {
       element.classList.add('none')
-      Header.classList.remove('none')
-      Footer.classList.remove('none')
     }
-    if (element.id === `Login`) {
-      Header.classList.add('none')
-      Footer.classList.add('none')
+
+    if (mainAMostrar === 'Login') {
+      Header.classList.add('none');
+      Footer.classList.add('none');
+    } else {
+      Header.classList.remove('none');
+      Footer.classList.remove('none');
     }
+    
   })
 }
+
+fetch("./Js/json/proyectos.json")
+  .then(response => response.json())
+  .then(dataProyectos => {
+    let Top3 = dataProyectos.sort((a, b) => b.cantVotos - a.cantVotos).slice(0,3);
+    mostrarTopDelMain(Top3)
+    mostrarListaProyectos(dataProyectos)
+  });
+
+fetch("./Js/json/proyectos.json")
+  .then(response => response.json())
+  .then(dataProyectos => {
+    let Top3 = dataProyectos.sort((a, b) => b.cantVotos - a.cantVotos).slice(0,3);
+    mostrarTopDelMain(Top3)
+    mostrarListaProyectos(dataProyectos)
+  });
+
+
+
+function mostrarTopDelMain(Top3) {
+  const Tops = document.getElementById('tops')
+  Tops.innerHTML = `
+    <article class="tops__article">
+        <div class="tops__article-divImg">
+          <img src="./Imagenes/FotoEjemplo.png" alt="" class="tops__article-divImg-img">
+        </div>
+        <div class="tops__article-lineas">
+          <div class="tops__article-lineas-circulo">1</div>
+        </div>
+        <div class="tops__article-divcard">
+          <div>
+            <p class="tops__article-divcard-div-text">
+              ${Top3[0].descripcion}
+            </p>
+          </div>
+          <div class="tops__article-divcard-buttons">
+            <button class="tops__article-divcard-buttons-button blanco">Votos: ${Top3[0].cantVotos}</button>
+            <button class="tops__article-divcard-buttons-button azul" id="buttonProyectoIndividual1">Ver
+              Proyecto</button>
+          </div>
+        </div>
+      </article>
+
+      <article class="tops__article reverse">
+        <div class="tops__article-divImg">
+          <img src="./Imagenes/FotoEjemplo.png" alt="" class="tops__article-divImg-img">
+        </div>
+        <div class="tops__article-lineas">
+          <div class="tops__article-lineas-circulo">2</div>
+        </div>
+        <div class="tops__article-divcard">
+          <div>
+            <p class="tops__article-divcard-div-text">
+              ${Top3[1].descripcion}
+            </p>
+          </div>
+          <div class="tops__article-divcard-buttons">
+            <button class="tops__article-divcard-buttons-button blanco">Votos: ${Top3[1].cantVotos}</button>
+            <button class="tops__article-divcard-buttons-button azul" id="buttonProyectoIndividual2">Ver
+              Proyecto</button>
+          </div>
+        </div>
+      </article>
+
+      <article class="article-wrapper">
+        <article class="tops__article">
+          <div class="tops__article-divImg">
+            <img src="./Imagenes/FotoEjemplo.png" alt="" class="tops__article-divImg-img">
+          </div>
+          <div class="tops__article-lineas">
+            <div class="tops__article-lineas-circulo">3</div>
+          </div>
+          <div class="tops__article-divcard">
+            <div>
+              <p class="tops__article-divcard-div-text">
+                ${Top3[2].descripcion}
+              </p>
+            </div>
+            <div class="tops__article-divcard-buttons">
+              <button class="tops__article-divcard-buttons-button blanco">Votos: ${Top3[2].cantVotos}</button>
+              <button class="tops__article-divcard-buttons-button azul" id="buttonProyectoIndividual3">Ver
+                Proyecto</button>
+            </div>
+          </div>
+        </article>
+        <div class="article-overlay">
+          <button class="btn-overlay" id="buttonMoreHome">Ver más</button>
+        </div>
+      </article>
+  
+  `
+}
+
+function mostrarListaProyectos(dataProyectos) {
+
+  dataProyectos.forEach(e => {
+
+    mainListaNormal.insertAdjacentHTML('beforeend', `
+    <article class="ListaNormal__article">
+      <div class="ListaNormal__article-div">
+        <img class="ListaNormal__article-div-img" src="./Imagenes/FotoEjemplo.png" alt="">
+      </div>
+      <div class="ListaNormal__article-div">
+        <div class="ListaNormal__article-div-div">
+          <h2>${e.nombre}</h2>
+          <div>
+            <img src="./Imagenes/Estrellas.png" alt="Imagen de estrella">
+            <img src="./Imagenes/Estrellas.png" alt="Imagen de estrella">
+            <img src="./Imagenes/Estrellas.png" alt="Imagen de estrella">
+            <img src="./Imagenes/Estrellas.png" alt="Imagen de estrella">
+            <img src="./Imagenes/Estrellas.png" alt="Imagen de estrella">
+          </div>
+        </div>
+        <div class="ListaNormal__article-div-div">
+          <p class="ListaNormal__article-div-div-p">${e.descripcion}</p>
+        </div>
+        <div class="ListaNormal__article-div-div">
+          <button id="VerProyecto1" class="ListaNormal__article-div-div-button">Ver Proyecto</button>
+        </div>
+      </div>
+    </article>`) 
+  });
+}
+
+
 
 
